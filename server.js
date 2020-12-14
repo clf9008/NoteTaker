7f7dfd37da40cac04420db766a88783ebaf65c1b
Follett <clf9008@gmail.com>
2020-12-14 10:49:44 -0700

    updated routes.js path to allow new notes to display and delete

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-  
-//Required Dependencies
+// Require Dependencies
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
 
-//Initialize express application
+// Initialize express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//Setup data parsing for express
+// Setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
 //Require routes file
-require('./routes/routes')(app);
+require('./db/routes/routes.js')(app);
 
-//Setup listener
+// Setup listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
