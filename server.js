365bc8014e5aabc212de9f62b1c5ed10126b6330
Follett <clf9008@gmail.com>
2020-12-14 10:59:51 -0700

    updated server.js error

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Required dependinces upon launch  
+//Required dependinces upon application launch  
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//data parsing 
+//data parsing method
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
@@ -15,7 +15,7 @@ app.use(express.static(__dirname));
 //require path for routes.js
 require('./db/routes/routes.js')(app);
 
-//Application listening even 
+//Application listening event 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
